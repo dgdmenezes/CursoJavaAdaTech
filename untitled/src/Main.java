6b47c86f9d75d2c53d4a34e4908151b61a1c370c
Dante Menezes <dgdmenezes@gmail.com>
2024-02-16 22:04:37 -0300

    Variáveis

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -3,9 +3,11 @@
 public class Main {
 
     public static void main(String[] args) {
-        int maximo =100;
+        int maximo =20;
+        String nome = "Dante";
 
         System.out.printf("Hello and welcome!\n");
+        System.out.printf("Meu nome é "+nome+"\n");
 
         for (int i = 1; i <= maximo; i++) {
             if (i%2 == 0){
@@ -14,7 +16,21 @@ public class Main {
             else{
                 System.out.println(i + " é ímpar");
             }
-
         }
-    }
+
+        int a;
+        int b = 2;
+        a = 3;
+        int soma = a + b;
+        int subtracao = a - b;
+        int multiplicacao = a * b;
+        float divisao = (float) a / b;
+
+
+
+        System.out.println("Soma de a + b é :" +soma);
+        System.out.println("Subtração de a - b é :" +subtracao);
+        System.out.println("Multiplicação de a x b é :" +multiplicacao);
+        System.out.println("Divisao de a / b é :" +divisao);
+       }
 }
