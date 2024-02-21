6620fe4d9b294437398b7a402398af41fb12d41d
Dante Menezes <dgdmenezes@gmail.com>
2024-02-21 09:08:32 -0300

    Estruturas Condicionais

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -32,5 +34,78 @@ public class Main {
         System.out.println("Subtração de a - b é :" +subtracao);
         System.out.println("Multiplicação de a x b é :" +multiplicacao);
         System.out.println("Divisao de a / b é :" +divisao);
-       }
+
+        //Operadores Boolean
+        boolean boleano = true;
+        boleano = false;
+
+        System.out.println("O boleano foi: " + boleano);
+        //Recomendação do Java é usar CamelCase
+        boolean fimDeSemana = true;
+        boolean fazendoSol = true;
+        boolean vamosAPraia = fimDeSemana && fazendoSol;
+
+        System.out.println("Vamos à praia? " + vamosAPraia);
+
+        //Tabela da Verdade
+        //Operador && (and)
+        //true && true = true
+        //false && true = false
+        //true && false = false
+        //false && false = false
+
+        //Operador || (or)
+        //true  || true = true
+        //false || true = true
+        //true  || false = true
+        //false || false = false
+
+        // Ternário
+
+        String mensagem = fimDeSemana?"É fim de semana": "Não é fim de semana";
+        // argumentação ? executa a 1ª condição se True:executa a 2ª condição se False
+
+        // Estruturas condicionais
+
+        int nota = 70;
+        int media = 70;
+
+        if (nota >=media){
+            System.out.println("Aluno tirou nota " + nota +" Está aprovado, pois é maior ou igual a" + media);
+        }else{
+            System.out.println("Aluno tirou nota " + nota + " Menor do que" + media + " logo está reprovado");
+        }
+
+        String graduacao = " ";
+        // A 80, B 70, C 60, D 0
+
+        if (nota >= 80){
+            graduacao = "A";
+        } else if (nota<80 && nota>=70) {
+            graduacao = "B";
+        } else if (nota<70&&nota>=60) {
+            graduacao = "C";
+        } else if (nota < 60 && nota >=0) {
+            graduacao = "D";
+        }else {
+            graduacao = null;
+        }
+
+        System.out.println("Nota tirada :" + nota + " Logo Graduação " + graduacao);
+
+        switch (graduacao){
+            case "A":
+            case "B":
+                System.out.println("Aluno aprovado");
+                break;
+            case "C":
+            case "D":
+                System.out.println("Aluno reprovado");
+                break;
+            default:
+                System.out.println("Nota inválida");
+        }
+
+
+    }
 }
